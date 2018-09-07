e211352e8b420c63c8c5853a07e7d148c4abde22
IT <IT@IT-PC42>
2018-09-07 09:33:55 +0530

    commit

---
+++ VendingMachine/src/VendingMachine.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class VendingMachine {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		System.out.println("1.Vaneela Rs:30.0");
+		System.out.println("2.Choclate Rs:45.0");
+		System.out.println("3.Strawberry Rs:35.0");
+		System.out.println("4.Butter-Scotch Rs:40.0");
+		int ch=s.nextInt();
+		Data d=new Data();
+		switch(ch)
+		{
+		case 1:d.func(30);break;
+		case 2:d.func(45);break;
+		case 3:d.func(35);break;
+		case 4:d.func(40);break;
+		default:System.out.println("Invalid Amount");
+		}
+		System.out.println("END");
+	}
+
+}
